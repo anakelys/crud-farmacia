134f1307a291e47d2a83397a41676cf76272e5b3
anakelys <souzanakely@gmail.com>
2025-03-19 12:58:28 -0300

    Crud do Produto

--- src/main/java/com/generation/crud_farmacia/model/Produto.java
+++ src/main/java/com/generation/crud_farmacia/model/Produto.java
@@ -1,6 +1,94 @@
 package com.generation.crud_farmacia.model;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
 public class Produto {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Size(min = 5, max = 100, message = "O nome tem que ser maior que 5 e menor que 100 caracteres")
+	private String nome;
+	
+	@NotNull(message = "O atributo quantidade é obrigatório")
+	private int quantidade;
+	
+	@NotNull(message = "O atributo preço é obrigatório")
+	private double preco; 
+	
+	@NotNull(message = "O atributo data de vencimento é obrigatório e tem formato: yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate datadevencimento;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
 
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public LocalDate getDatadevencimento() {
+		return datadevencimento;
+	}
+
+	public void setDatadevencimento(LocalDate datadevencimento) {
+		this.datadevencimento = datadevencimento;
+	}
+
+
+	
 }
